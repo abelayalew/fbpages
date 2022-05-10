387f2dcaa29cef51fa23a46d7e2d91a651ce500a
abelayalew <abelayalew81@gmail.com>
2022-05-10 22:48:32 +0300

    scrapper update

--- scraper.py
+++ scraper.py
@@ -1,6 +1,6 @@
 from fbpages import models
 from facebook_scraper import get_posts
-from telegram import Bot
+from telegram import Bot, ParseMode
 from decouple import config
 
 
@@ -16,12 +16,12 @@ for page in models.FacebookPage.objects.all():
         
         page.last_update = int(post['post_id'])
         
-        text = f"#{page.name}\n\n" + post['post_text'][:800]
+        text = f"#{page.name}\n\n" + post['post_text'][:800] + f"\n\n<a href='{post['post_url']}'>click here to read more</a>"
         
         for subscriber in subscribers:
             
             if post['image']:
-                bot.sendPhoto(chat_id=subscriber.id, photo=post['image'], caption=text)
+                bot.sendPhoto(chat_id=subscriber.id, photo=post['image'], caption=text, parse_mode=ParseMode.HTML)
             
             else:
-                bot.sendMessage(chat_id=subscriber.id, text=text)
+                bot.sendMessage(chat_id=subscriber.id, text=text, parse_mode=ParseMode.HTML)
